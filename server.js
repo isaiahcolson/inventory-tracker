62e05be102146f9887f87bb923ab7ec5c8408e4c
j0nathanp3nEnterprise <jonathanpen1230@gmail.com>
2020-05-30 13:23:00 -0400

    added root route, works

--- server.js
+++ server.js
@@ -1,8 +1,30 @@
+// External module
 const express = require('express');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+
+// Internal Module
+const controllers = require('./controllers');
+
+// Instance module
 const app = express();
 
-const PORT = 4000;
+// System config variable
+const PORT = process.env.PORT || 4000;
 
+// App configuration
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(methodOverride('_method'));
+
+// Root route
+app.get('/', (req,res) =>{
+    res.render('index');
+});
+
+// Bind server to PORT
 app.listen(PORT, function(){
-    console.log(`Local port is running at http://localhost:${PORT}`);
-})
+    console.log(`Server is live on http://localhost:${PORT}`);
+});
