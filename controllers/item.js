5eaa9b2747857b1b42580daab9048b36812b2853
j0nathanp3nEnterprise <jonathanpen1230@gmail.com>
2020-06-01 14:24:02 -0400

    added items ref to models/List.js and lists to models/Item.js

--- controllers/item.js
+++ controllers/item.js
@@ -3,8 +3,19 @@ const router = express.Router();
 
 const db = require('../models');
 
+// Show route
 router.get('/', (req,res) => {
-    res.send('items/index');
+    db.Item.findById(req.params.id, function(err, allItems) {
+        if (err) {
+            console.log(err);
+            res.send({message: 'Internal server error.'});
+        } else {
+            const context = {items: allItems};
+            res.render('items/show', context);
+        }
+    });
 });
+
+
 
 module.exports = router;
--- models/Item.js
+++ models/Item.js
@@ -5,7 +5,11 @@ const itemSchema = new mongoose.Schema ({
     category: {type:String, required:true},
     price: {type:Number, required:true},
     quantity: {type:Number, required:true},
-    reorderLevel: {type:Number, required:true}
+    reorderLevel: {type:Number, required:true},
+    list: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'List'
+    }
 }, {timestamps:true});
 
 const Item = mongoose.model('Item', itemSchema);
