617e66fceba4d57c5237a88fa06c30541a8acfe6
j0nathanp3nEnterprise <jonathanpen1230@gmail.com>
2020-05-31 20:01:37 -0400

    adds mongodb connection

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const connectionString = "mongodb://localhost:27017/inventory";
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(function () {
+    console.log("Mongodb connected connection successful.");
+  })
+  .catch(function (err) {
+    console.log("Mongodb connection error", err);
+  });
+
+module.exports = {
+    List: require ('./List')
+};
