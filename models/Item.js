96b610501dd389b8e4bf32c86f391ff1fe245bf5
j0nathanp3nEnterprise <jonathanpen1230@gmail.com>
2020-06-01 13:48:02 -0400

    item.js created, note that items code will start to work when item routers are created

---
+++ models/Item.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const itemSchema = new mongoose.Schema ({
+    name: {type:String, required:true},
+    category: {type:String, required:true},
+    price: {type:Number, required:true},
+    quantity: {type:Number, required:true},
+    reorderLevel: {type:Number, required:true}
+}, {timestamps:true});
+
+const Item = mongoose.model('Item', itemSchema);
+
+module.exports = Item;
+
