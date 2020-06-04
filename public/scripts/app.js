9b97c73b7cd5317673cb9e61e51375a782cf3f7c
colsonisaiah <colson.isaiah@gmail.com>
2020-06-03 23:10:17 -0400

    dark theme based on user preferences

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -10,7 +10,33 @@ $('.modal-bg').click(function(){
     $('.modal-container').toggleClass('hidden');
 });
 
+// $('#dark').click(function(){
+//     console.log('adjust');
+//     $('body').toggleClass('dark');
+// });
+
+const color = $('.dash-main').css('background-color');
+
+const themeSwitch = () => {
+    if (color == 'rgb(14, 19, 26)') {
+        // console.log('working');
+        $('body').toggleClass('light');
+    } else if (color == 'rgb(241, 241, 241)') {
+        // console.log('working');
+        // console.log(color);
+        $('body').toggleClass('dark');
+    } else {
+        console.log('not-working');
+    }
+}
+
+const initTheme = () => {
+    console.log(localStorage);
+}
+
+// const themeSwitch = $('#dark');
+
 $('#dark').click(function(){
-    console.log('adjust');
-    $('body').toggleClass('dark');
+    themeSwitch();
+    initTheme();
 });
