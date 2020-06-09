6e8b02ad583243192cc7c90dcd4c1f37a5c1bff7
j0nathanp3nEnterprise <jonathanpen1230@gmail.com>
2020-06-09 16:01:00 -0400

    solve git models/index.js issue

--- models/index.js
+++ models/index.js
@@ -1,20 +1,15 @@
 const mongoose = require('mongoose');
-<<<<<<< HEAD
+
 require("dotenv").config();
 
 const connectionString = 
   process.env.MONGODB_URI;
 
-=======
 // using .envfile
 require("dotenv").config();
 
-<<<<<<< HEAD
-const conenctionString = process.env.MONGODB_URI;
->>>>>>> 44064a0f60ed5a5dd71754844eb62453fb224415
-=======
 const connectionString = process.env.MONGODB_URI;
->>>>>>> f3afaf86f8d6775362ae67afdcb1000997569054
+
 mongoose
   .connect(connectionString, {
     useNewUrlParser: true,
